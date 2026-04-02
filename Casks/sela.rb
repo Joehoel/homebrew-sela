e3163f83c7cb20b990d6bd0ad892f1b40e5b4b9a
Joël Kuijper <jmkuijper@outlook.com>
2026-04-02 19:02:11 +0200

    Lower minimum macOS to Sonoma (14) for broader compatibility

--- Casks/sela.rb
+++ Casks/sela.rb
@@ -7,7 +7,7 @@ cask "sela" do
   desc "Translate ProPresenter worship songs from English to Dutch"
   homepage "https://github.com/Joehoel/sela"
 
-  depends_on macos: ">= :sequoia"
+  depends_on macos: ">= :sonoma"
   depends_on arch: :arm64
 
   app "Sela.app"
